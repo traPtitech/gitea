c3b9db7ef8b973cb05b5842680d4b51b1644e178
sapphi-red <green@sapphi.red>
2020-02-24 12:35:04 +0900

    traQのアイコンを使用

--- models/user/avatar.go
+++ models/user/avatar.go
@@ -62,6 +62,14 @@ func (u *User) AvatarLinkWithSize(ctx context.Context, size int) string {
 		return avatars.DefaultAvatarLink()
 	}
 
+	// return traQ avatar if u is individual
+	if !u.IsOrganization() {
+		if u.Name == "gitea-actions" {
+			return "/assets/img/avatar_default.png"
+		}
+		return "https://q.trap.jp/api/v3/public/icon/" + u.Name
+	}
+
 	useLocalAvatar := false
 	autoGenerateAvatar := false
 
