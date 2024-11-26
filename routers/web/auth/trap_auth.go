9dd3c38d8ba6dbe8dad17eb2cb1272c927d2a5a3
pirosiki197 <lingmukangtai825@gmail.com>
2024-11-26 19:21:02 +0900

    emailをportalと同期する

--- routers/web/auth/trap_auth.go
+++ routers/web/auth/trap_auth.go
@@ -49,14 +49,17 @@ func getUserFromTrapToken(ctx *context.Context, tokenString string) *user.User {
 	}
 
 	data := token.Claims.(jwt.MapClaims)
-	log.Debug("traP token accepted: %s", data["id"])
+	id := data["id"].(string)
+	email := data["email"].(string)
+	log.Debug("traP token accepted: %s", id)
 
-	u, _ := user.GetUserByName(ctx, data["id"].(string))
+	u, _ := user.GetUserByName(ctx, id)
 	if u == nil {
 		u = &user.User{
-			Name:   data["id"].(string),
-			Email:  data["email"].(string),
-			Passwd: "",
+			Name:     id,
+			Email:    email,
+			Passwd:   "",
+			IsActive: true,
 		}
 		overwrite := &user.CreateUserOverwriteOptions{
 			IsRestricted: optional.Some(false),
@@ -74,6 +77,11 @@ func getUserFromTrapToken(ctx *context.Context, tokenString string) *user.User {
 	}
 	if err := user_service.UpdateUser(ctx, u, &opts); err != nil {
 		log.ErrorWithSkip(3, "Failed to update user: %v", err)
+		return nil
+	}
+
+	if err := user_service.ReplacePrimaryEmailAddress(ctx, u, email); err != nil {
+		log.ErrorWithSkip(3, "Failed to update email: %v", err)
 		return nil
 	}
 
